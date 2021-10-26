97942c5855af94c3e18c7070f02121abb8aca04f
Goi <goizaneolaneta@gmail.com>
2021-10-26 22:14:39 +0200

    Scanner Update

---
+++ teoria/TryCatch/ScannerMain.java
@@ -0,0 +1,22 @@
+package com.openbootcamp.teoria.TryCatch;
+
+import java.util.Scanner;
+
+public class ScannerMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un nombre");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Introduce un número");
+        int numero = scanner.nextInt();
+
+        System.out.println("El número introducido es: " + nombre);
+        System.out.println("El número introducido es: " + numero);
+
+        // System.out.println("Hola mundo");
+    }
+}
