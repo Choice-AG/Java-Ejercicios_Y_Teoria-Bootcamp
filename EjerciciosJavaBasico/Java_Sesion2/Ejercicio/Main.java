ff281ef3eb431c025ed16b4b9f7a89c3b6fa331b
Goi <goizaneolaneta@gmail.com>
2021-12-08 18:50:20 +0100

    08/12

--- EjerciciosJavaBasico/Java_Sesion2/Ejercicio/Main.java
+++ EjerciciosJavaBasico/Java_Sesion2/Ejercicio/Main.java
@@ -8,7 +8,7 @@ import jdk.swing.interop.SwingInterOpUtils;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static <String> void main(String[] args) {
 
 
         double precio = 67.4;
